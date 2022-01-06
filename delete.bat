c35319b36ebd692d837a36a338f98c14b12a2e2c
rnjstjdgh <gshgsh0831@gmail.com>
2022-01-06 14:37:02 +0900

    first commit

---
+++ delete.bat
@@ -0,0 +1,8 @@
+winsw stop winsw_prometheus.xml
+winsw uninstall winsw_prometheus.xml
+
+winsw stop winsw_grafana.xml
+winsw uninstall winsw_grafana.xml
+
+@REM winsw stop winsw_alertmanager.xml
+@REM winsw uninstall winsw_alertmanager.xml
---
+++ install.bat
@@ -0,0 +1,12 @@
+winsw install winsw_prometheus.xml
+winsw start winsw_prometheus.xml
+
+winsw install winsw_grafana.xml
+winsw start winsw_grafana.xml
+
+winsw install winsw_middle_server.xml
+winsw start winsw_middle_server.xml
+
+@REM winsw install winsw_alertmanager.xml
+@REM winsw start winsw_alertmanager.xml
+
