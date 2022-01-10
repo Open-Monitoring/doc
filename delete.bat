199973f03e232ae5e4199a6f53ef03f21a37de49
rnjstjdgh <gshgsh0831@daum.net>
2022-01-10 22:37:52 +0900

    middle-server 삭제 스크립트 수정

--- delete.bat
+++ delete.bat
@@ -4,5 +4,8 @@ winsw uninstall winsw_prometheus.xml
 winsw stop winsw_grafana.xml
 winsw uninstall winsw_grafana.xml
 
+winsw stop winsw_middle_server.xml
+winsw uninstall winsw_middle_server
+
 @REM winsw stop winsw_alertmanager.xml
 @REM winsw uninstall winsw_alertmanager.xml
